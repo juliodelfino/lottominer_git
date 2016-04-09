f3980f0b292673d9e37b041fa78c87b3faf960e2
juliodelfino <julius.delfino@yahoo.com>
2016-04-09 12:46:47 +0800

    Draft for mail functionality.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,11 @@
+require 'sendgrid-ruby'
+
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
     @results = LottoResult.all
+    
+    send_lotto_mail
   end
   
   def show
@@ -24,5 +28,21 @@ class ArticlesController < ApplicationController
   private
     def article_params
       params.require(:article).permit(:title, :text)
+    end
+    
+    def send_lotto_mail
+  
+      client = SendGrid::Client.new(api_key: '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')
+      
+      mail = SendGrid::Mail.new do |m|
+        m.to = 'jhackr@gmail.com'
+        m.from = 'taco@cat.limo'
+        m.subject = 'Notification from Lottominer'
+        m.text = 'I heard you like pineapple.'
+      end
+    
+      res = client.send(mail)
+      @resp = LottoResult.create(game: res.code, numbers: res.body, won: false)
+      @resp.save
     end
 end
