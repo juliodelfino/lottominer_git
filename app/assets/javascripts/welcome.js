05b199fe1eee31f9e9edc6a8997b1be1fe7572c3
juliodelfino <julius.delfino@yahoo.com>
2016-05-14 13:40:05 +0800

    Added datepicker

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
 
-
+	var lottoDate = $("#datepicker").datepicker({
+      showOn: "button",
+      buttonImage: "assets/images/calendar.gif",
+      buttonImageOnly: true,
+      buttonText: "Select date",
+      dateFormat: 'yymmdd',
+      defaultDate: getParameterByName('date') != null ? getParameterByName('date') : new Date(),
+      onSelect: function(date) {
+      	window.location = '?date=' + date;
+      }
+    });
 });
