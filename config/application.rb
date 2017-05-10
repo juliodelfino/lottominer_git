de2430818e3c6086520a82060fbdd02050da90bc
juliodelfino <julius.delfino@yahoo.com>
2017-05-11 02:43:58 +0800

    Rails 5 updates

--- config/application.rb
+++ config/application.rb
@@ -19,9 +19,11 @@ module Lottominer
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
-    config.autoload_paths << Rails.root.join('lib')
-    config.autoload_paths << Rails.root.join('lib//pcso')
+ 
+    config.autoload_paths << "#{Rails.root}/lib"
+    config.autoload_paths << "#{Rails.root}/lib/pcso"
+    config.eager_load_paths << "#{Rails.root}/lib"
+    config.eager_load_paths << "#{Rails.root}/lib/pcso"
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     
