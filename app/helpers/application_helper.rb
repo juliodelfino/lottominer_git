35d119ce80330bb71d9695626aa40d566e70a6ac
juliodelfino <julius.delfino@yahoo.com>
2017-05-11 09:10:27 +0800

    Added small dashes in between numbers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def ball_view(txt_numbers)
-    return raw txt_numbers.split('-').map{|x| '<span class="ball">' + x + '</span>'}.join();
+    return raw txt_numbers.split('-').map{|x| '<span class="ball">' + x + '</span>'}.join('<span class="small-dash">-</span>');
   end
 end
 
