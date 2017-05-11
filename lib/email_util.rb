f75384bd1aa6f3454d7e31a33581484649772307
juliodelfino <julius.delfino@yahoo.com>
2017-05-12 07:53:39 +0800

    Fixed SendGrid email, updated home page

--- lib/email_util.rb
+++ lib/email_util.rb
@@ -2,6 +2,8 @@ class EmailUtil
     
   def self.send(mail_info)
     
+    
+=begin
     client = SendGrid::Client.new(api_key: ENV['SENDGRID_API_KEY'])
     
     mail = SendGrid::Mail.new do |m|
@@ -15,5 +17,20 @@ class EmailUtil
     res = client.send(mail)
     puts "Sending mail status: " + res.code.to_s
     return res.code.to_s
+=end
+
+
+    from = SendGrid::Email.new(email: mail_info[:from])
+    subject = mail_info[:subject]
+    to = SendGrid::Email.new(email: mail_info[:to])
+    content = SendGrid::Content.new(type: 'text/html', value: mail_info[:html])
+    mail = SendGrid::Mail.new(from, subject, to, content)
+
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
+
+    puts "Sending mail status: " + response.status_code.to_s
+    return response.status_code.to_s
+
   end
 end
